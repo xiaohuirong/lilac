3ab20f515fb50af05768b7a2c3eaaf33f516bc20
hoream <2175223953@qq.com>
2023-05-16 16:13:15 +0800

    modify qcm-git

--- archoream/qcm-git/lilac.py
+++ archoream/qcm-git/lilac.py
@@ -4,6 +4,6 @@ def pre_build():
     aur_pre_build()
 
     for line in edit_file('PKGBUILD'):
-        if line.startswith('_info'):
-            line = line.replace('_info', 'echo')
+        if line.startswith('    _info'):
+            line = line.replace('    _info', '    echo')
         print(line)
