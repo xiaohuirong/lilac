fb015594f8e93eb4e1344b86018514bf9fd618ed
hoream <2175223953@qq.com>
2023-09-22 00:08:13 +0800

    update

---
+++ archoream/gnome-terminal-transparency/lilac.py
@@ -0,0 +1,10 @@
+from lilaclib import aur_pre_build, edit_file
+
+def pre_build():
+    aur_pre_build()
+
+    for line in edit_file('PKGBUILD'):
+        if line.startswith('groups='):
+            pass
+        else:
+            print(line)
