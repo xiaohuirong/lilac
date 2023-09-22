6ac23ba7ac5b60dd2688b19b9511f69069d14569
hoream <2175223953@qq.com>
2023-09-22 10:41:12 +0800

    update

---
+++ archoream/librewolf-bin/lilac.py
@@ -0,0 +1,11 @@
+from lilaclib import *
+
+def pre_build():
+    #aur_pkgver, aur_pkgrel = get_pkgver_and_pkgrel()
+    aur_pre_build()
+    #for line in edit_file('PKGBUILD'):
+    #    if line.startswith('pkgrel='):
+    #        print(f'aur_pkgrel={aur_pkgrel}')
+    #    print(line)
+    
+
