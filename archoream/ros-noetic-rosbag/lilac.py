db3c93165abb024edbddc4f4b25a8c870cfbcc0c
hoream <2175223953@qq.com>
2023-09-28 01:45:09 +0800

    fix ros-noetic-rosbag

--- archoream/ros-noetic-rosbag/lilac.py
+++ archoream/ros-noetic-rosbag/lilac.py
@@ -5,5 +5,5 @@ def pre_build():
 
     for line in edit_file('PKGBUILD'):
         print(line)
-        if line.startswith('build() {'):
-            print("sed -i '1i\#define BOOST_TIMER_ENABLE_DEPRECATED' ${srcdir}/ros_comm-${pkgver}/tools/rosbag/src/encrypt.cpp")
+        #if line.startswith('build() {'):
+        #    print("sed -i '1i\#define BOOST_TIMER_ENABLE_DEPRECATED' ${srcdir}/ros_comm-${pkgver}/tools/rosbag/src/encrypt.cpp")
