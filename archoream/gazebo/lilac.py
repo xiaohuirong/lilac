afb9f02dbc65fcc63e2f074e890e374e7e5d6a06
hoream <2175223953@qq.com>
2023-09-28 22:24:12 +0800

    fix gazebo

--- archoream/gazebo/lilac.py
+++ archoream/gazebo/lilac.py
@@ -1,7 +1,7 @@
 from lilaclib import aur_pre_build, edit_file
 
 def pre_build():
-    aur_pre_build()
+    #aur_pre_build()
 
     for line in edit_file('PKGBUILD'):
         if line.startswith('build() {'):
