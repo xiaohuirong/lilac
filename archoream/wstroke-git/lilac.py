74a0c43276681d7ced87b981c46786f37b82040a
hoream <2175223953@qq.com>
2023-10-18 16:54:47 +0800

    update

---
+++ archoream/wstroke-git/lilac.py
@@ -0,0 +1,10 @@
+from lilaclib import aur_pre_build, edit_file
+
+def pre_build():
+    aur_pre_build()
+
+    for line in edit_file('PKGBUILD'):
+        if line.startswith('depends=('):
+            print(r'''depends=('wayfire-git' 'nlohmann-json' 'glib2' 'gtk3' 'gtkmm3' 'boost-libs')''')
+            continue
+        print(line)
