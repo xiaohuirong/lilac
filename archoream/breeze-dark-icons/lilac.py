a36f31fcdffbfc8cc1cc854cb3fbdac55a5f4041
hoream <2175223953@qq.com>
2023-10-19 15:50:55 +0800

    add breeze-dark-icons

---
+++ archoream/breeze-dark-icons/lilac.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from lilaclib import *
+
+def try_remove_pkgrel(newver):
+    match = re.search(r'-\d+$', newver)
+
+    if match:
+        result = newver[:match.start()]
+    else:
+        result = newver
+
+    return result
+
+def pre_build():
+  update_pkgver_and_pkgrel(try_remove_pkgrel(_G.newver))
+
+def post_build():
+  git_add_files('PKGBUILD')
+  git_commit()
+# vim:set ts=2 sw=2 et:
+
+
