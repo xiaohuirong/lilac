d7c032affb966168f76eefa199aff85feff7be2c
hoream <2175223953@qq.com>
2023-10-20 12:09:40 +0800

    add obs-rachni-theme-larger-font

---
+++ archoream/obs-rachni-theme-larger-font/lilac.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from lilaclib import *
+
+def try_remove_pkgrel(newver):
+    match = re.search(r'-\d+$', newver)
+
+    if match:
+        result = newver[:match.start()]
+    else:
+        result = newver
+
+    return result
+
+def pre_build():
+  update_pkgver_and_pkgrel(try_remove_pkgrel(_G.newver))
