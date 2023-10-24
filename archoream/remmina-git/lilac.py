1a27e3975071271e9f3381a424d6219574b69419
hoream <hoream@qq.com>
2023-10-24 21:33:41 +0800

    fix remmina-git

---
+++ archoream/remmina-git/lilac.py
@@ -0,0 +1,9 @@
+from lilaclib import aur_pre_build, edit_file
+
+def pre_build():
+    aur_pre_build()
+
+    for line in edit_file('PKGBUILD'):
+        if r'''"kwallet"''' in line:
+            line = line.replace(r'''"kwallet"''', r'''"kwallet5"''')
+        print(line)
