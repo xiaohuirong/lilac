6eea3cee8e1010e79f098049ddf60688a197d1e1
hoream <hoream@qq.com>
2023-10-25 00:15:42 +0800

    fix

--- archoream/remmina-git/lilac.py
+++ archoream/remmina-git/lilac.py
@@ -2,6 +2,7 @@ from lilaclib import aur_pre_build, edit_file
 
 def pre_build():
     aur_pre_build()
+    update_pkgver_and_pkgrel(_G.newver)
 
     for line in edit_file('PKGBUILD'):
         if r'''"kwallet"''' in line:
