a39bc6bce00a495837cf2cf40fdfcb1871e1fe6d
hoream <hoream@qq.com>
2023-10-28 20:29:11 +0800

    fix wine-lol

---
+++ archoream/wine-lol/lilac.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from lilaclib import *
+import re
+
+def try_remove_pkgrel_and_epoch(newver):
+    version_pattern = r'\d+:(\S+)'
+
+    match = re.search(version_pattern, newver)
+    if match:
+        version = match.group(1)
+        return version
+    else:
+        return newver
+
+def pre_build():
+    aur_pre_build()
+    
+    if _G.newver is not None: 
+        update_pkgver_and_pkgrel(try_remove_pkgrel_and_epoch(_G.newver))
