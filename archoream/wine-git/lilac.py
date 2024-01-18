8deb610dafbc62121ce15212c1dac1f9bf8ec391
hoream <hoream@qq.com>
2024-01-18 19:59:09 +0800

    wine-git enable wayland

--- archoream/wine-git/lilac.py
+++ archoream/wine-git/lilac.py
@@ -2,7 +2,7 @@ from lilaclib import aur_pre_build, edit_file, add_makedepends, add_depends
 
 def pre_build():
     aur_pre_build()
-    add_depends(['wayland', 'lib32-wayland'])
+    add_depends(['wayland', 'lib32-wayland', 'libxkbcommon'])
     add_makedepends(['wayland-protocols', 'pkgconf'])
 
     for line in edit_file('PKGBUILD'):
