9814e11256f440febb9b4a55278841a49d960ced
hoream <hoream@qq.com>
2024-04-05 14:51:55 +0800

    rebuild wide-dhcpv6

---
+++ archoream/wide-dhcpv6/lilac.py
@@ -0,0 +1,9 @@
+from lilaclib import aur_pre_build, edit_file
+
+def pre_build():
+    aur_pre_build()
+
+    for line in edit_file('PKGBUILD'):
+        print(line)
+        if line.startswith('options='):
+            print('makedepends=(patch, bison, flex)')
