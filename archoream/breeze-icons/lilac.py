a1aa354289ff54ef6d4f0ac1bfad52339dd11042
hoream <hoream@qq.com>
2024-05-18 01:06:25 +0800

    rebuild breeze-icons

--- archoream/breeze-icons/lilac.py
+++ archoream/breeze-icons/lilac.py
@@ -13,7 +13,6 @@ def try_remove_pkgrel(newver):
     return result
 
 def pre_build():
-  add_makedepends(['extra-cmake-modules'])
   if _G.newver is not None:
       update_pkgver_and_pkgrel(try_remove_pkgrel(_G.newver))
 
