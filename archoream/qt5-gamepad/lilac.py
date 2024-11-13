97dd2d68de68e1cd2f8ade90a4fafb51565826c9
hoream <hoream@qq.com>
2024-11-13 17:46:59 +0800

    update

--- archoream/qt5-gamepad/lilac.py
+++ archoream/qt5-gamepad/lilac.py
@@ -3,3 +3,8 @@ from lilaclib import *
 def pre_build():
     aur_pre_build()
     run_cmd(['updpkgsums'])
+
+    for line in edit_file('PKGBUILD'):
+        if line.startswith('groups=('):
+            continue
+        print(line)
