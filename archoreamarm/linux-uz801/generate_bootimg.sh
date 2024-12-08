b11137e7e5c9fff1900e437ca509871c999b8e52
hoream <hoream@qq.com>
2024-12-08 15:05:44 +0800

    add generate boot.img scripts

---
+++ archoreamarm/linux-uz801/generate_bootimg.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cat /boot/Image.gz /boot/dtbs/qcom/msm8916-yiming-uz801v3.dtb > /boot/zImage.gz
+echo "Successful generate /boot/zImage.gz"
+
+mkbootimg \
+    --base 0x80000000 \
+    --kernel_offset 0x00080000 \
+    --ramdisk_offset 0x02000000 \
+    --tags_offset 0x01e00000 \
+    --pagesize 2048 \
+    --second_offset 0x00f00000 \
+    --ramdisk /boot/initramfs-linux-uz801.img \
+    --cmdline "earlycon root=PARTUUID=a7ab80e8-e9d1-e8cd-f157-93f69b1d141e console=ttyMSM0,115200 no_framebuffer=true rw" \
+    --kernel /boot/zImage.gz \
+    -o /boot/boot.img
+
+echo "Successful generate /boot/boot.img"
+rm /boot/zImage.gz
+echo "Remove /boot/zImage.gz"
