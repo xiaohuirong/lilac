d427d3cefb53c2d0bbf815a06e10c89e4484ec9b
hoream <hoream@qq.com>
2024-12-13 15:04:01 +0800

    fix

--- archoream/openvino/lilac.py
+++ archoream/openvino/lilac.py
@@ -2,5 +2,5 @@ from lilaclib import *
 
 def pre_build():
     aur_pre_build()
-    run_cmd(['git config http.proxy http://chat.hoream.top:8889'])
+    # run_cmd(['git config http.proxy http://chat.hoream.top:8889'])
 
