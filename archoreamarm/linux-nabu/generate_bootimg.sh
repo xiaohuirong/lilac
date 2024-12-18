adc2f8e19985c72aafddaf32f4251ec3ebce1680
hoream <hoream@qq.com>
2024-12-18 15:13:16 +0800

    add linux-nabu

---
+++ archoreamarm/linux-nabu/generate_bootimg.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cat /boot/Image.gz /boot/dtbs/qcom/sm8150-xiaomi-nabu.dtb > /boot/zImage.gz
+echo "Successful generate /boot/zImage.gz"
+
+# mkbootimg \
+#     --base 0x80000000 \
+#     --kernel_offset 0x00080000 \
+#     --ramdisk_offset 0x02000000 \
+#     --tags_offset 0x01e00000 \
+#     --pagesize 2048 \
+#     --second_offset 0x00f00000 \
+#     --ramdisk /boot/initramfs-linux-uz801.img \
+#     --cmdline "earlycon root=PARTUUID=a7ab80e8-e9d1-e8cd-f157-93f69b1d141e console=ttyMSM0,115200 rw zswap.enabled=0" \
+#     --kernel /boot/zImage.gz \
+#     -o /boot/boot.img
+
+mkbootimg \
+    --kernel zImage \
+    --cmdline "pd_ignore_unused clk_ignore_unused console=tty0 root=UUID=4175779f-0a77-4771-b035-308c38cd2356 rw rootwait" \
+    --base 0x00000000 \
+    --kernel_offset 0x00008000 \
+    --tags_offset 0x00000100 \
+    --pagesize 4096 \
+    --id \
+    -o /boot/boot.img
+
+echo "Successful generate /boot/boot.img"
+rm /boot/zImage.gz
+echo "Remove /boot/zImage.gz"
